a0e090cef94edf4f71e0a0edded242be0264ae93
Jonathan Bartlett <jwb133@googlemail.com>
2017-04-27 11:53:24 +0100

    Adding documentation to the case cohort and nested case control example datasets, and moving the documentation for example datasets into a new file R/data.r

--- data-raw/cc_example.r
+++ data-raw/cc_example.r
@@ -1,6 +1,7 @@
 #the following example is not run when the package is compiled on CRAN
 #(to keep computation time down), but it can be run by package users
 \dontrun{
+  #as per the documentation for ex_cc, the sampling fraction is 10%
   imps <- smcfcs.casecohort(ex_cc, smformula="Surv(entertime, t, d)~x+z", sampfrac=0.1,
                             in.subco="in.subco", method=c("", "", "norm", "", "", "", ""))
   library(mitools)
