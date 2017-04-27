4a705494d2e55a461ba110430b92f58c6664ce69
Jonathan Bartlett <jwb133@googlemail.com>
2017-04-27 13:43:28 +0100

    Breaking lines in documentation to make them shorter.

--- data-raw/ncc_example.r
+++ data-raw/ncc_example.r
@@ -4,8 +4,10 @@
   predictorMatrix <- matrix(0,nrow=dim(ex_ncc)[2],ncol=dim(ex_ncc)[2])
   predictorMatrix[which(colnames(ex_ncc)=="x"),c(which(colnames(ex_ncc)=="z"))] <- 1
 
-  imps <- smcfcs.nestedcc(originaldata=ex_ncc,set="setno",nrisk="numrisk",event="d",smformula="Surv(t,case)~x+z+strata(setno)",
-                          method=c("", "", "logreg", "", "", "", "", ""),predictorMatrix=predictorMatrix)
+  imps <- smcfcs.nestedcc(originaldata=ex_ncc,set="setno",nrisk="numrisk",event="d",
+                          smformula="Surv(t,case)~x+z+strata(setno)",
+                          method=c("", "", "logreg", "", "", "", "", ""),
+                          predictorMatrix=predictorMatrix)
   library(mitools)
   impobj <- imputationList(imps$impDatasets)
   models <- with(impobj, clogit(case~x+z+strata(setno)))
