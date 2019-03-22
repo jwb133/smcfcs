7b074c337c697dd98a319428ade7199abd7821a7
Jonathan Bartlett <jwb67@bath.ac.uk>
2019-03-22 16:13:31 +0000

    Adding statistical tests for covariate measurement error functionality.

---
+++ tests/testthat/test_measerror.r
@@ -0,0 +1,62 @@
+library(smcfcs)
+library(survival)
+library(mitools)
+context("Covariate measurement error testing")
+
+test_that("Linear regression with cov. measurement error is consistent", {
+  skip_on_cran()
+  expect_equal({
+    set.seed(1234)
+
+    n<- 10000 #sample size
+    x <- rnorm(n)
+    w1 <- x+rnorm(n)
+    w2 <- x+rnorm(n)
+    y <- x+rnorm(n)
+    x <- rep(NA, n)
+
+    simData <- data.frame(x,w1,w2,y)
+
+    errMat <- matrix(0, nrow=4, ncol=4)
+    errMat[1,c(2,3)] <- 1
+
+    imps <- smcfcs(simData, smtype="lm", smformula="y~x",
+                   method=c("latnorm", "", "", ""),
+                   errorProneMatrix=errMat,numit=100,m=1)
+    abs(coef(lm(y~x, data=imps$impDatasets[[1]]))[2]-1)<0.05
+}, TRUE)
+  })
+
+test_that("Linear regression with cov. measurement error coverage is ok", {
+  skip_on_cran()
+  expect_equal({
+    library(mitools)
+    set.seed(1234)
+    nSim <- 1000
+    n <- 1000
+    ests <- array(0, dim=c(nSim,2))
+
+    for (i in 1:nSim) {
+      x <- rnorm(n)
+      w1 <- x+rnorm(n)
+      w2 <- x+rnorm(n)
+      w2[round(n*0.1):n] <- NA
+      y <- x+rnorm(n)
+      x <- rep(NA, n)
+
+      simData <- data.frame(x,w1,w2,y)
+
+      errMat <- matrix(0, nrow=4, ncol=4)
+      errMat[1,c(2,3)] <- 1
+
+      imps <- smcfcs(simData, smtype="lm", smformula="y~x",
+                     method=c("latnorm", "", "", ""),
+                     errorProneMatrix=errMat,numit=100,m=5)
+      impobj <- imputationList(imps$impDatasets)
+      models <- with(impobj, lm(y~x))
+      ests[i,] <- c(summary(MIcombine(models))[2,3], summary(MIcombine(models))[2,4])
+    }
+    #check coverage is close to 95%
+    abs(mean((ests[,1]<1) & (ests[,2]>1))-0.95)<(qnorm(0.99)*((0.95*0.05)/nSim)^0.5)
+}, TRUE)
+  })
