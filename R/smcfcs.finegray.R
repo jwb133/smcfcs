b6bde694359cf37e6ba0d5a709652aa5473cb62f
efbonneville <e.f.bonneville@lumc.nl>
2024-05-23 22:28:28 +0200

    Adding some checks

--- R/smcfcs.finegray.R
+++ R/smcfcs.finegray.R
@@ -1,16 +1,26 @@
 #' Substantive model compatible fully conditional specification imputation of
 #' covariates for a Fine-Gray model
 #'
+#' Multiply imputes missing covariate values using substantive model compatible
+#' fully conditional specification for competing risks outcomes, when the
+#' substantive model is a Fine-Gray model for the subdistribution hazard of one event.
+#'
+#' In the presence of random right censoring, the function first multiply imputes
+#' the potential censoring times for those failing from competing events using
+#' \link[kmi]{kmi}, and thereafter uses \link[smcfcs]{smcfcs} to impute the missing
+#' covariates. See Bonneville \emph{et al.} 2024 for further details on the methodology.
+#'
 #' @author Edouard F. Bonneville \email{e.f.bonneville@@lumc.nl}
 #'
 #' @param smformula A formula of the form "Surv(t, d) ~ x1 + x2", where t
 #' is a vector of competing event times, and d is a (numeric) competing event
 #' indicator, where 0 must designate a censored observation.
-#' @param cause Numeric, designating the competing event of interest.
+#' @param cause Numeric, designating the competing event of interest (default is
+#' `cause = 1`).
 #' @param kmi_args List, containing arguments to be passed on to \link[kmi]{kmi}.
 #' The "formula" element is a formula where the right-hand side specifies the
 #' covariates used for multiply imputing the potential censoring times for
-#' individual's failing from competing events. The default is formula = ~ 1,
+#' individual's failing from competing events. The default is `formula = ~ 1`,
 #' which uses marginal Kaplan-Meier estimator of the censoring distribution.
 #' @param ... Additional arguments to pass on to \link[smcfcs]{smcfcs}
 #' @inheritParams smcfcs
@@ -19,10 +29,33 @@
 #' \link[smcfcs]{smcfcs}.
 #' @export
 #'
+#' @references Bonneville EF, Beyersmann J, Keogh RH, Bartlett JW, Morris TP,
+#' Polverelli N, de Wreede LC, Putter H. Multiple imputation of missing covariates
+#' when using the Fine--Gray model. 2024. Submitted.
+#'
 #' @examples
 #' \dontrun{
-#' #...
+#' library(survival)
+#' library(kmi)
+#'
+#' imps <- smcfcs.finegray(
+#'   originaldata = ex_finegray,
+#'   smformula = "Surv(times, d) ~ x1 + x2",
+#'   method = c("", "", "logreg", "norm"),
+#'   cause = 1,
+#'   kmi_args = list("formula" = ~ 1)
+#' )
+#'
+#' if (requireNamespace("mitools", quietly = TRUE)) {
+#'   library(mitools)
+#'   impobj <- imputationList(imps$impDatasets)
+#'   # Important: use Surv(newtimes, newevent) ~ ... when pooling
+#'   # (respectively: subdistribution time and indicator for cause of interest)
+#'   models <- with(impobj, coxph(Surv(newtimes, newevent) ~ x1 + x2))
+#'   summary(MIcombine(models))
 #' }
+#' }
+#'
 smcfcs.finegray <- function(originaldata,
                             smformula,
                             method,
@@ -43,12 +76,14 @@ smcfcs.finegray <- function(originaldata,
 
   # Locate/sort out outcome variables
   outcome_vars <- all.vars(update(as.formula(smformula), . ~ 1))
-  time_var_name <- head(outcome_vars, n = 1L) # will need to add warning/error if (tstart, tstop) format
-  status_var_name <- tail(outcome_vars, n = 1L)
+  time_var_name <- outcome_vars[[1]] # will need to add warning/error if (tstart, tstop) format
+  status_var_name <- outcome_vars[[2]]
   time_var <- originaldata[[time_var_name]]
   status_var <- originaldata[[status_var_name]]
-  if (!is.numeric(status_var))
-    stop("Status variable should be numeric, with 0 indicating a censored observation.")
+
+  # Some checks
+  checkmate::assert_numeric(x = time_var, lower = 0, finite = TRUE, any.missing = FALSE)
+  checkmate::assert_numeric(x = status_var, lower = 0, finite = TRUE, any.missing = FALSE)
 
   # Get functional form RHS of formula
   smformula_rhs <- unlist(strsplit(smformula, split = "~"))[2]
@@ -91,6 +126,22 @@ smcfcs.finegray <- function(originaldata,
     # Prepare kmi() formula (for now default Kaplan-Meier imputation)
     lhs_kmi <- paste0("Surv(", paste(outcome_vars, collapse = ", "), " != 0)")
     form_kmi <- as.formula(paste0(lhs_kmi, deparse1(kmi_args$formula)))
+    rhs_cens <- all.vars(kmi_args$formula)
+
+    # Some checks for the censoring
+    if (length(rhs_cens) > 0) {
+      if (any(!(rhs_cens %in% colnames(originaldata))))
+        stop("Variables used to model the censoring distribution need to be in originaldata.")
+      if (anyNA(originaldata[, rhs_cens]))
+        warning(
+          "          One or more of the variables used to model the censoring
+          distribution has missing data. This MI method works under the assumption
+          that only complete covariates can be associated with the censoring. If
+          nothing is changed by the user, {kmi} deals with these missing values
+          internally by using single imputation."
+        )
+    }
+
     args_cens_imps <- c(
       list(
         "formula" = form_kmi,
@@ -103,7 +154,6 @@ smcfcs.finegray <- function(originaldata,
     )
 
     # Impute missing censoring times in first loop
-    # We use the local timefixed version (in smcfcs package: edit survfit as globally new fun with timefix = FALSE)
     kmi_imps <- do.call(kmi::kmi, args = args_cens_imps)
 
     # And now impute the covariates
